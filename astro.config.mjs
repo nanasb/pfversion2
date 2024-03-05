c93fdd938724ebbb58fec93a5ee414ee82d379ba
NanaSouaibou825 <bouharinanasouaibou@gmail.com>
2024-03-06 02:01:37 +0300

    intallation de netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
 import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 
 
 // https://astro.build/config
@@ -9,5 +10,6 @@ export default defineConfig({
   integrations: [icon()],
   output: 'server',
   adapter: vercel(),
+  adapter: netlify(),
 });
 
