bd113820643d0dae6d2ff41c2fb58cacec43249c
NanaSouaibou825 <bouharinanasouaibou@gmail.com>
2024-03-06 03:51:11 +0300

    Fix adapter configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-import icon from "astro-icon";
-import vercel from '@astrojs/vercel/serverless';
+import icon from 'astro-icon';
 import netlify from '@astrojs/netlify';
-
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
   output: 'server',
-  adapter: vercel(),
   adapter: netlify(),
 });
+
 
