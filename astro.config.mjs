271dd7245f19e17083d067eff67d869e79ad5c51
NanaSouaibou825 <bouharinanasouaibou@gmail.com>
2024-03-18 03:24:59 +0300

    Votre message de commit ici

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  output: 'server',
-  adapter: vercel(),
-  // et moi
-  buildOptions: {
-    out: 'dist',
-  },
 });
 
 
